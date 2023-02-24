035ada944f5414fd9791f748d66da3b333a00054
gmorales-github <gustavom.morales@gmail.com>
2023-02-23 22:58:57 -0300

    Se sube el proyecto final para su entrega

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const menuBtn = document.querySelector('#menu-button');
+const menuItems = document.querySelectorAll(".menu-item");
+
+
+/* Botón hamburguesa mobile */
+menuBtn.addEventListener('click', function() {
+    document.body.classList.toggle('mobile-menu-active');
+});
+
+
+/* Hover de inicio y contacto */
+menuItems.forEach(function(item) {
+      
+    item.addEventListener('click', function() {
+     
+      document.body.classList.remove('mobile-menu-active');
+
+      const currentItem = document.querySelector('.active');
+      currentItem.classList.remove('active');
+      this.classList.add('active');
+    });
+});
+
